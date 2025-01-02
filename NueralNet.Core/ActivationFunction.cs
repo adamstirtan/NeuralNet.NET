3975c0980c40ed56b0181412fe72eaf07d771092
Adam Stirtan <adamstirtan@gmail.com>
2025-01-02 11:26:41 -0500

    Add project files.

---
+++ NueralNet.Core/ActivationFunction.cs
@@ -0,0 +1,23 @@
+﻿namespace NueralNet.Core;
+
+public class ActivationFunction
+{
+    private string _name;
+
+    public ActivationFunction(string name, Func<double, double> function, Func<double, double> derivative)
+    {
+        _name = name;
+
+        Function = function;
+        Derivative = derivative;
+    }
+
+    public Func<double, double> Function { get; }
+    public Func<double, double> Derivative { get; }
+
+    public static ActivationFunction Sigmoid => new("Sigmoid", x => 1 / (1 + Math.Exp(-x)), x => x * (1 - x));
+
+    public static ActivationFunction Tanh => new("Tanh", x => Math.Tanh(x), x => 1 - Math.Pow(x, 2));
+
+    public static ActivationFunction ReLU => new("ReLU", x => Math.Max(0, x), x => x > 0 ? 1 : 0);
+}
---
+++ NueralNet.Core/Neuron.cs
@@ -0,0 +1,60 @@
+﻿namespace NueralNet.Core;
+
+public class Neuron
+{
+    private static readonly Random _random = new();
+
+    public Neuron(int inputCount)
+    {
+        Weights = new(inputCount);
+
+        for (int i = 0; i < inputCount; i++)
+        {
+            Weights.Add(_random.NextDouble() * 2 - 1);
+        }
+
+        Bias = _random.NextDouble() * 2 - 1;
+    }
+
+    public List<double> Weights { get; private set; }
+    public double Bias { get; private set; }
+    public double Output { get; private set; }
+
+    /// <summary>
+    /// Calculates the output of the neuron given a list of inputs and an activation function.
+    /// </summary>
+    /// <param name="inputs"></param>
+    /// <param name="activationFunction"></param>
+    /// <returns></returns>
+    public double CalculateOutput(List<double> inputs, ActivationFunction activationFunction)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            sum += inputs[i] * Weights[i];
+        }
+
+        sum += Bias;
+
+        Output = activationFunction.Function(sum);
+
+        return Output;
+    }
+
+    /// <summary>
+    /// Updates the weights and bias of the neuron given the gradients and a learning rate.
+    /// </summary>
+    /// <param name="weightGradients"></param>
+    /// <param name="biasGradient"></param>
+    /// <param name="learningRate"></param>
+    public void UpdateParameters(List<double> weightGradients, double biasGradient, double learningRate)
+    {
+        for (int i = 0; i < Weights.Count; i++)
+        {
+            Weights[i] -= learningRate * weightGradients[i];
+        }
+
+        Bias -= learningRate * biasGradient;
+    }
+}
